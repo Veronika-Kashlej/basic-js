a315f5be507540795488f19698037be9196431a6
Veronika Kashlej <veronika.kashlei@mail.ru>
2024-12-18 19:18:59 +0300

    feat: 49passing

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const arr = [];
+  let count = 1;
+  for (let i = 1; i < str.length; i += 1) {
+    if (str[i] === str[i - 1]) {
+      count += 1;
+      if (i === str.length - 1) {
+        arr.push(`${count}${str[i]}`);
+      }
+    } else {
+      if (i === str.length - 1 && str[i] !== str[i - 1]) {
+        arr.push(`${count === 1 ? "" : count}${str[i - 1]}${str[i]}`);
+      } else {
+        arr.push(`${count === 1 ? "" : count}${str[i - 1]}`);
+      }
+      count = 1;
+    }
+  }
+  return arr.join("");
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let result = n;
+  while (result / 10 >= 1) {
+    let str = result.toString();
+    result = 0;
+    for (let i = 0; i < str.length; i += 1) {
+      result += +str[i];
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
